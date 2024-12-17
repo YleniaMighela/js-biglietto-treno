d42fb567c2fced0cce424ada29d6bd0e8882c721
Ylenia <yleniamighela@gmail.com>
2024-12-17 15:47:23 +0100

    [fix] add main variables

--- script.js
+++ script.js
@@ -3,3 +3,33 @@
 // - va applicato uno sconto del 20% per i minorenni
 // - va applicato uno sconto del 40% per gli over 65.
 // - L’output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+
+
+// DATI UTENTI
+
+// Chiediamo all'utente il numero di km da percorrere 
+let chilometri = parseInt(prompt("Inserire quanti chilometri si vogliono percorrere "));
+
+// Chiediamo all'utente l'età del passeggero
+let eta = parseInt(prompt("Inserire età passeggero"))
+
+// Prezzo del biglietto
+const prezzochilometro = 0.21;
+
+// Calcoliamo il prezzo del biglietto in base ai km
+
+
+
+// In base all'età del passeggero applichiamo gli sconti
+  // SE è minore di 18 anni avrà lo sconto del 20%
+    
+
+  // SE è maggiore di 65 anni avrà lo sconto del 40%
+
+
+  // SE il passeggero ha un'età tra i 18 e i 64 anni pagherà prezzo pieno
+
+
+// OUTPUT 
+// Prezzo totale
+    
